dd0aed4effda7f8d37a733a48db7051d30e1e229
Christiana <christiana.john1@gmail.com>
2021-10-05 11:09:14 +0530

    Problem 6

--- 6.py
+++ 6.py
@@ -16,16 +16,24 @@ import sys
 
 def plusMinus(arr):
     # Write your code here
-    pos=0
-    neg=0
-    z=0
-    for i in arr:
-        if(i>0):pos+=1
-        elif(i<0):neg+=1
-        elif(i==0): z+=1
-    print(round((pos/n),6))
-    print(round((neg/n),6))
-    print(round((z/n),6))
+    # pos=0
+    # neg=0
+    # z=0
+    # for i in arr:
+    #     if(i>0):pos+=1
+    #     elif(i<0):neg+=1
+    #     elif(i==0): z+=1
+    # print(round((pos/n),6))
+    # print(round((neg/n),6))
+    # print(round((z/n),6))
+
+    for i in range(len(arr)):
+        if(arr[i]>0):arr[i]=1
+        elif(arr[i]<0):arr[i]=-1
+    print(round((arr.count(1)/n),6))
+    print(round((arr.count(-1)/n),6))
+    print(round((arr.count(0)/n),6))
+
 
 if __name__ == '__main__':
     n = int(input().strip())
